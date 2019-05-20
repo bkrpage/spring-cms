372b913614af452782f57d871baf08a7901f0ec2
Bradley Page <bkrpage94@gmail.com>
2019-05-20 01:28:15 +0100

    Spring Security configured for REST login

--- src/main/java/dev/bradleypage/config/SecurityConfiguration.java
+++ src/main/java/dev/bradleypage/config/SecurityConfiguration.java
@@ -1,16 +1,60 @@
 package dev.bradleypage.config;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 
+/**
+ * Using https://www.baeldung.com/securing-a-restful-web-service-with-spring-security
+ */
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final RestAuthenticationEntryPoint restAuthenticationEntryPoint;
+    private final CustomSavedRequestAwareAuthenticationSuccessHandler customSavedRequestAwareAuthenticationSuccessHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(encoder().encode("adminPass")).roles("ADMIN")
+                .and()
+                .withUser("user").password(encoder().encode("userPass")).roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll();
-        http.cors().and().csrf().disable();
+        http
+                .csrf().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(restAuthenticationEntryPoint)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/").authenticated()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
+                .successHandler(customSavedRequestAwareAuthenticationSuccessHandler)
+                .failureHandler(failureHandler())
+                .and()
+                .logout();
+    }
+
+    @Bean
+    public SimpleUrlAuthenticationFailureHandler failureHandler(){
+        return new SimpleUrlAuthenticationFailureHandler();
     }
 }
